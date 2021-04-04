c0e0d681016a61e635d6a61d636db7af47c85e1a
RadovanTomik <67159739+RadovanTomik@users.noreply.github.com>
2021-04-04 17:00:33 +0200

    Added installs

--- debian_script.sh
+++ debian_script.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo"HelloWorld!"
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo"{GREEN}Installing apps...{NC≈} "
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install net-tools
+sudo apt-get install curl
+sudo apt-get install wget
+sudo apt-get install w3m
+sudo apt-get install ufw
+sudo apt-get install docker
+sudo apt-get install docker-compose
